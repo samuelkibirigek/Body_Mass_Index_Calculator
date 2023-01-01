e7aa6eb82d5bda8c197ff93ac6e9775ab43edb8b
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-01-01 17:16:36 +0300

    Initial commit for body mass index calculator.

---
+++ main.py
@@ -0,0 +1,8 @@
+
+height = float(input("Enter your height in m: \n"))
+weight = float(input("Enter your weight in kg: \n"))
+
+bmi = int(weight / (height ** 2))
+
+
+print(f"Your Body Mass Index is {bmi} and you are ")
